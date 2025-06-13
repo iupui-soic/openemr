5def9f3acf4bbd5973ccf55967a5d275ddda7051
drlalithapranathi <lapula@iu.edu>
2025-06-13 00:47:56 -0400

    style: added fixed window size test_utils.py

--- oemr-selenium/test_utils.py
+++ oemr-selenium/test_utils.py
@@ -89,6 +89,7 @@ def init_browser():
     if os.environ.get('HEADLESS', 'false').lower() == 'true':
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument('--window-size=1920,1080')
         options.add_argument("--disable-dev-shm-usage")
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
